ae16d47a79927f3763448df666ea7f5e03b0600b
Nero-He <kagaminenyanya@163.com>
2021-05-22 17:17:35 +0800

    update and fixed

--- FilesToSort/sort_files_1.py
+++ FilesToSort/sort_files_1.py
@@ -5,8 +5,6 @@ import shutil
 def main():
     os.chdir('FilesToSort')
     for filename in os.listdir('.'):
-        if os.path.isdir(filename):
-            continue
         extent = filename.split('.')[-1]
         os.mkdir(extent)
         print("{}/{}".format(extent, filename))
